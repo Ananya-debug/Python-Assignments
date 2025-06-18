f1e26443f5d0279586e2101d75ef586f71e6c041
Ananya Chakraborty <105624688+Ananya-debug@users.noreply.github.com>
2025-06-18 13:16:36 +0530

    yes

--- 3.py
+++ 3.py
@@ -10,7 +10,7 @@ odd_selector = [num % 2 != 0 for num in numbers]
 even = list(compress(numbers,even_selector))
 odd = list(compress(numbers,odd_selector))
 
-#duck typing -any iterable can be passed
+#duck typing - any iterable can be passed
 
 def print_iterable(it):
      for i in it:
